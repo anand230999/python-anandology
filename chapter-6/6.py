d91fa02ce6adfd3c925604ddd9638e14900dde89
Anand Krishnan <anand230999@gmail.com>
2016-02-02 15:59:46 +0530

    Create 6.py

---
+++ chapter-6/6.py
@@ -0,0 +1,27 @@
+def json_encode(data):
+    if isinstance(data, bool):
+        if data:
+            return "true"
+        else:
+            return "false"
+    elif isinstance(data, (int, float)):
+        return str(data)
+    elif isinstance(data, str):
+        return '"' + escape_string(data) + '"'
+    elif isinstance(data, list):
+        return "[" + ", ".join(json_encode(d) for d in data) + "]"
+    elif isinstance(data,dict):
+        r="{"
+        for k,v in data.items():
+            r= r+json_encode(k)+":"+str(son_encode(v))+","
+        r=r+"}"
+        return r
+    else:
+        raise TypeError("%s is not JSON serializable" % repr(data))
+
+def escape_string(s):
+    """Escapes double-quote, tab and new line characters in a string."""
+    s = s.replace('"', '\\"')
+    s = s.replace("\t", "\\t")
+    s = s.replace("\n", "\\n")
+    return s
